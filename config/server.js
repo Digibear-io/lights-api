b3fa43758969408a6888827ce0b505d0468d03e8
= <=>
2020-12-18 05:04:18 -0800

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a652b32cc9516f60007429d41b07be97'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1089cf5c-e4e4-4ba7-9fa9-5cd794b66f05'
+};
